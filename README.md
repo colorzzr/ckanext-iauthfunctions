3a31795ca7804c5cdf3c92e3332320f2e7a8fb5f
colorzzr <colorzzr@gmail.com>
2019-05-13 17:10:31 -0400

    update project for setting access to the about page

--- README.md
+++ README.md
@@ -4,27 +4,31 @@ ckan extension for setting access for the route `/about`
 
 # Overview
 
-The extension set the access for the `/about` page. Only the user name as `hp`<bold>(not uppercase)</bold> can see the `/about` page
+The extension set the access for the `/about` page. Only the user name as `hp` **(not uppercase)** can see the `/about` page
+
+But note that **admin_user** always have the access to all the stuff, so you may want to use the normal user
 
 # Set up
 
-1. run the `python setup.py develop` to install the plugin
+1. download the extension using 
 
-2. add the `ckanext-iauthfunctions` to `ckan.plugins` attribute in the `development.ini` file
+2. run the `python setup.py develop` to install the plugin
 
-3. replace the function `def about()` in file `ckan/ckan/views/home.py` below:
+3. add the `ckanext-iauthfunctions` to `ckan.plugins` attribute in the `development.ini` file
+
+4. replace the function `def about()` in file `ckan/ckan/views/home.py` below:
 
 ```
 def about():
     u''' display about page'''
     try:
-        p.toolkit.get_action('my_auth')(context, data_dict)
+        p.toolkit.get_action('my_auth')(None, None)
     except p.toolkit.NotAuthorized:
         p.toolkit.abort(403, 'Not authorized to see this tab')
 
     return base.render(u'home/about.html', extra_vars={})
 ```
 
-4. then run the command `paster serve /etc/ckan/default/development.ini` <bold>under the virtual environment</bold>
+5. then run the command `paster serve /etc/ckan/default/development.ini` **under the virtual environment**
 
 
--- ckanext/iauthfunctions/plugin.py
+++ ckanext/iauthfunctions/plugin.py
@@ -1,87 +1,19 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
-from ckan.common import c, request
-# from ckan.common import 
+from ckan.common import c
 
-# def tag_show_test(context, data_dict=None):
-# 	return {'success' : False}
-
+# This the hooked autherization function
 def my_auth(context, data_dict=None):
-	print("-------my_auth------")
-	print(request.url)
 	if c.user != 'hp':
 		return {'success' : False}
 	else:
 		return {'success' : True}
 
-	# return {'success' : False}
 
-
+# This is the function that autherization hooked and will be called in the about function
 def my_auth_func(context, data_dict=None):
-	# print("-------my_auth_func------")
-	# if user are not logined it would directly fail
 	toolkit.check_access('my_auth', context, None)
 	
-
-
-def site_read_test(context, data_dict=None):
-	print('------site_read_test------')
-	# print(context['user'])
-	# note here if user are not login then the user attribute IS NOT in the CONTEXT!
-	try:
-		print("test!" + request.url)
-		if context['user'] != 'hp':
-			if request.url == 'http://localhost:5000/about':
-				return {'success' : False}
-			else:
-				return {'success' : True}
-		else:
-			return {'success':True}
-
-	except toolkit.NotAuthorized:
-		# p.toolkit.abort will log user out when auth function fails, which may not be desired
-		print("------test------")
-		toolkit.abort(403, 'Not authorized to see this tab')
-
-def group_create(context, data_dict=None):
-	# Get the user name of the logged-in user.
-	user_name = context['user']
-	# print("------context------")
-	# print(context)
-	# print(context['model'])
-
-	# Get a list of the members of the 'curators' group.]
-	try:
-		members = toolkit.get_action('member_list')(
-		data_dict={'id': 'onlyicanadd', 'object_type': 'user'})
-		# print(members)
-	except toolkit.ObjectNotFound:
-		# The curators group doesn't exist.
-		return {'success': False,
-		'msg': "The curators groups doesn't exist, so only sysadmins "
-		"are authorized to create groups."}
-
-	# 'members' is a list of (user_id, object_type, capacity) tuples, we're
-	# only interested in the user_ids.
-	member_ids = [member_tuple[0] for member_tuple in members]
-	# print(member_ids)
-
-	# We have the logged-in user's user name, get their user id.
-	convert_user_name_or_id_to_id = toolkit.get_converter(
-	'convert_user_name_or_id_to_id')
-	user_id = convert_user_name_or_id_to_id(user_name, context)
-	# print(user_id)
-
-	# print(type(user_id), type(member_ids[0]))\
-	
-	# Finally, we can test whether the user is a member of the curators group.
-	if user_id in member_ids:
-		# print("in")
-		return {'success': True}
-	else:
-		# print("not in")
-		return {'success': False,
-		'msg': 'Only curators are allowed to create groups'}
 
 class IauthfunctionsPlugin(plugins.SingletonPlugin):
 	plugins.implements(plugins.IAuthFunctions)
@@ -90,11 +22,8 @@ class IauthfunctionsPlugin(plugins.SingletonPlugin):
 	# IAuthFunctions
 	def get_auth_functions(self):
 		return {
-			'group_create': group_create,
-			# 'site_read':site_read_test,
 			'my_auth': my_auth,
 		}
-
 
 	# IActions
 	def get_actions(self):
